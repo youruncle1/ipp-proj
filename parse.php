f06d2d7bad21714ceb0033d05daa1e0d38823fe8
your_uncle <xpolia05@vutbr.cz>
2023-04-08 20:09:17 +0200

    restored parse and readme1 to match submitted versions

--- parse.php
+++ parse.php
@@ -19,29 +19,17 @@ function parsearguments(){
         echo "Usage: php8.1 parse.php [OPTIONS]\n";
         echo "Options:\n";
         echo "  --help          Display this help message and exit.\n";
-        /*echo "  --stats=file    Specify a file to write statistics to.\n";
-        echo "  --loc           Count lines of code.\n";
-        echo "  --comments      Count lines with comments.\n";
-        echo "  --labels        Count unique labels.\n";
-        echo "  --jumps         Count jump instructions.\n";
-        echo "  --fwjumps       Count forward jump instructions.\n";
-        echo "  --backjumps     Count backward jump instructions.\n";
-        echo "  --badjumps      Count bad jump instructions(jumps to undef. label).\n";
-        echo "  --frequent      Find most frequently used instructions.\n";
-        echo "  --print         Print out a string into statistics file.\n";
-        echo "  --eol           Print out an end-of-line into statistics file.\n";
-        */
         exit(0);
     }
-}
+}#parsearguments()
 
-
+#kontrola hlavicky .IPPcode23
 function headercheck($file){
     while(true){
         $line = fgets($file);
-        $header = explode("#", $line);
+        $header = explode("#", $line); 
         $header = trim($header[0]);
-        if (empty($header))
+        if (empty($header)) #preskoci prazdne/komentovane riadky pred hlavickou
             continue;
         break;
     }
@@ -50,31 +38,34 @@ function headercheck($file){
         fwrite(STDERR, "Error: incorrect or missing header in trhe source code written in IPPcode23." . PHP_EOL);
         exit(21);
     }
+}#headercheck()
 
-}
-
+#upravi nacitany riadok tak, aby sa mohol predat analyzatoru
 function contentcut(&$line){
-    $line = preg_replace('!\s+!', ' ', $line);
+    $line = preg_replace('!\s+!', ' ', $line); #zamena duplikatov whitespaces za singularny
     $line = trim($line);
     $line = explode('#', $line);
     $line = $line[0];
-    $line = trim($line);
-}
+    $line = trim($line); #potrebny trim, ak bol komentar oddeleny medzerou od zdrojoveho kodu
+}#contentcut()
 
+#lex. kontrola neterminalu <var>
 function isvar($token){
     if (preg_match( "/^(LF|TF|GF)@[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*$/", $token))
         return true;
     else
         return false;
-}
+}#isvar()
 
+#lex. kontrola neterminalu <type>
 function istype($token){
     if (preg_match("/^(bool|int|string)$/", $token))
         return true;
     else
         return false;
-}
+}#istype()
 
+#lex. kontrola neterminalu <symb>
 function issymb($token){
     if (isvar($token)){
         return "var";
@@ -94,15 +85,17 @@ function issymb($token){
     else{
         return false;
     }
-}
+}#issymb()
 
+#lex. kontrola neterminalu <label>
 function islabel($token){
     if (preg_match("/^[a-zA-Z\-_$&%*!?][a-zA-Z0-9\-_$&%*!?]*$/", $token))
         return true;
     else
         return false;
-}
+}#islabel()
 
+#XML zapis pre <var>|<label>
 function XMLaddVarLabel(&$XML, &$instruction, &$instructcnt, $token, $type){
     $instruction = $XML->addChild('instruction');
     $instruction->addAttribute('order', $instructcnt);
@@ -110,8 +103,9 @@ function XMLaddVarLabel(&$XML, &$instruction, &$instructcnt, $token, $type){
     $arg1 = $instruction->addChild('arg1', htmlspecialchars($token[1], ENT_XML1, 'UTF-8'));
     $arg1->addAttribute('type', $type);
     $instructcnt++;
-}
+}#XMLaddVarLabel()
 
+#XML zapis pre <symb>
 function XMLaddSymb(&$XML, &$instruction, $token, $symb, $argn){
     switch($symb){
         case 'var':
@@ -128,16 +122,18 @@ function XMLaddSymb(&$XML, &$instruction, $token, $symb, $argn){
             $arg2->addAttribute('type', $symb);
             break;
     }
-}
+}#XMLaddSymb()
 
+
+#hlavna funkcia analyzatoru
 function parse($file){
     
     headercheck($file);
 
     $XML = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><program></program>');
     $XML->addAttribute('language', 'IPPcode23');
-    $instruction;
-    $instructcnt = 1;
+    $instruction; #uklada aktualne nacitanu instrukciu
+    $instructcnt = 1; #counter instrukcii
     
     while ($line = fgets(STDIN)){
         
@@ -147,7 +143,8 @@ function parse($file){
             continue;
 
         $token = explode(' ', $line);
-        $token[0] = strtoupper($token[0]);
+        $token[0] = strtoupper($token[0]); #token[0] == instrukcia
+        
         switch($token[0]){
             #### <> ####
             case 'CREATEFRAME':
@@ -275,10 +272,9 @@ function parse($file){
         }#switch
     }#while
     echo $XML->asXML();
-}#func
+}#parse()
 
 parsearguments();
 parse(STDIN);
-
 
 ?>
--- readme1.md
+++ readme1.md
@@ -1,15 +1,14 @@
-# Implementačná dokumentácia k 1. úlohe IPP 22/23
-Meno a Priezvisko: Roman Poliačik\
-login: __xpolia05__
+# Implementační dokumentace k 1. úloze do IPP 2022/2023
+Jméno a příjmení: Roman Poliačik
+Login: xpolia05
 
 ### 1. Úvod
-
+g
 Úlohou bolo implementovať skript typu filter parse.php v jazyku PHP8.1, ktorý číta zdrojový kód v IPPcode23 zo STDIN, skontroluje kód z hľadiska lexikálnej a syntaktickej správnosti a vypíše ho v štandardnej reprezentácii XML na STDOUT.
 
 ### 2. Implementácia
 
 Základom riešenia je použitie vstavanej triedy `SimpleXMLElement`, ktorá zabezpečuje zostavenie a generovanie kódu XML.\
 Hlavnou funkciou skriptu je funkcia `parse()`, v ktorej sa prv skontroluje správnosť hlavičky funkciou `headercheck()` a vytvorí nový objekt `XML` triedy `SimpleXMLElement`. Syntaktická kontrola prebieha formou konečného automatu implementovaného pomocou cyklu a prepínača. Každou iteráciou sa pomocou `fgets()` načíta jeden riadok zo vstupu, vymažú sa z neho prebytočné whitespaces a komentáre funkciou `contentcut()`.\ 
-V následujúcom prepínači sú inštrukcie jazyka IPPcode23 rozdelené do skupín podľa počtu a druhov operandov. Na základe typu načítanej inštrukcie sa overí správny počet operandov a ich správnosť funkciami `isvar()`, `issymb()`, `islabel()` alebo `istype()` využívajúc porovnania regulárnych výrazov(regex matching). Pre regexovú kontrolu čísiel som použil na hexadecimálne a oktálové čísla regexový zápis z oficiálnej stránky PHP *php.net*. Ak prešla kontrola, kombináciou `XMLaddVarLabel()`, `XMLaddSymb()` a prípadných špeciálnych prípadov sa zapíšu do objektu `XML` pomocou funkcií jeho triedy 
-potrebné parametre. Nakoniec, ak nedošlo k chybe, sa na štandardný výstup vypíše reprezentáciu programu v XML.\
+V následujúcom prepínači sú inštrukcie jazyka IPPcode23 rozdelené do skupín podľa počtu a druhov operandov. Na základe typu načítanej inštrukcie sa overí správny počet operandov a ich správnosť funkciami `isvar()`, `issymb()`, `islabel()` alebo `istype()` využívajúc porovnania regulárnych výrazov(regex matching). Pre regexovú kontrolu čísiel som použil na hexadecimálne a oktálové čísla regexový zápis z oficiálnej stránky PHP *php.net*. Ak prešla kontrola, kombináciou `XMLaddVarLabel()`, `XMLaddSymb()` a prípadných špeciálnych prípadov sa zapíšu do objektu `XML` pomocou funkcií jeho triedy potrebné parametre. Nakoniec, ak nedošlo k chybe, sa na štandardný výstup vypíše reprezentácia programu v XML a skript vráti hodnotu 0.\
 Všetky chybové hlásenia sa vypíšu na štandardný chybový výstup. 
