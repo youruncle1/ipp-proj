2f7093b8d2251370846edf3bbfeb60116495d91c
your_uncle <xpolia05@vutbr.cz>
2023-04-09 16:24:05 +0200

    added XMLValidator

--- interpret.py
+++ interpret.py
@@ -1,5 +1,8 @@
 import argparse
 import sys
+import re
+import xml.etree.ElementTree as ET
+
 
 def argparser():
     parser = argparse.ArgumentParser(
@@ -46,9 +49,145 @@ def argparser():
         parser.error("At least one of the parameters (--source or --input) must always be specified.")
 
     return args
+class Instruction:
+    def __init__(self, order, opcode, args):
+        self.order = order
+        self.opcode = opcode
+        self.args = args
+
+class Argument:
+    def __init__(self, arg_type, value):
+        self.arg_type = arg_type
+        self.value = value
+
+class XMLValidator:
+    def __init__(self, xml_string):
+        try:
+            self.root = ET.fromstring(xml_string)
+        except ET.ParseError:
+            self.root = None
+        
+        self.valid_opcodes = {
+            'CREATEFRAME': 0, 'PUSHFRAME': 0, 'POPFRAME': 0, 'RETURN': 0, 'BREAK': 0,
+            'DEFVAR': 1, 'POPS': 1, 'CALL': 1, 'LABEL': 1, 'JUMP': 1, 'PUSHS': 1, 'WRITE': 1, 'EXIT': 1, 'DPRINT': 1,
+            'MOVE': 2, 'INT2CHAR': 2, 'NOT': 2, 'STRLEN': 2, 'TYPE': 2, 'READ': 2,
+            'ADD': 3, 'SUB': 3, 'MUL': 3, 'IDIV': 3, 'LT': 3, 'GT': 3, 'EQ': 3, 'AND': 3, 'OR': 3, 'STRI2INT': 3, 'CONCAT': 3, 'GETCHAR': 3, 'SETCHAR': 3, 'JUMPIFEQ': 3, 'JUMPIFNEQ': 3
+        }
+        
+        self.valid_argtypes = {
+            "var": r"^(LF|TF|GF)@[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*$",
+            "type": r"^(bool|int|string)$",
+            "label": r"^[a-zA-Z\-_$&%*!?][a-zA-Z0-9\-_$&%*!?]*$",
+            "nil": r"^nil$",
+            "string": r"^([^\\]|\\\d{3})*$",
+            "bool": r"^(true|false)$",
+            "int": r"^(?:\+|-)?(?:(?!.*_{2})(?!0\d)\d+(?:_\d+)*|0[oO]?[0-7]+(_[0-7]+)*|0[xX][0-9a-fA-F]+(_[0-9a-fA-F]+)*)$"
+        }
+
+    def check_header(self):
+        if self.root is None:
+            return 31, "XML parse error"
+        if self.root.tag != "program" or self.root.get("language") != "IPPcode23":
+            return 32, "Invalid program header"
+        return 0, None
+
+    def parse_instruction(self, xml_instruction):
+        order = xml_instruction.get("order")
+        if order is None or not order.isdigit() or int(order) < 1:
+            return None, 32
+        order = int(order)
+
+        opcode = xml_instruction.get("opcode")
+        if not opcode or opcode not in self.valid_opcodes:
+            return None, 32
+        required_args = self.valid_opcodes[opcode]
+        args = []
+
+        arg_tags = {}
+        for arg in xml_instruction:
+            if not re.match(r'arg[123]$', arg.tag):
+                return None, 32
+            arg_index = int(arg.tag[-1])
+            if arg_index in arg_tags:
+                return None, 32
+            arg_tags[arg_index] = arg
+
+        if len(arg_tags) != required_args:
+            return None, 32
+
+        for i in range(1, len(arg_tags) + 1):
+            arg = arg_tags.get(i)
+            if arg is None:
+                return None, 32
+            arg_type = arg.get("type")
+
+            # Check if arg_type is in valid_argtypes (valid_argtypes)
+            if arg_type not in self.valid_argtypes:
+                return None, 32
+
+            arg_value = arg.text
+
+            # Check if arg_value matches the corresponding regex pattern for the arg_type
+            pattern = self.valid_argtypes.get(arg_type)
+            if not re.match(pattern, arg_value):
+                return None, 32
+
+            args.append(Argument(arg_type, arg_value))
+
+        return Instruction(order, opcode, args), 0
+
+    def validate_instructions(self):
+        instructions = []
+        orders = []
+
+        for xml_instruction in self.root:
+            if xml_instruction.tag == "instruction":
+                instruction, error_code = self.parse_instruction(xml_instruction)
+                if error_code:
+                    return None, error_code, f"Invalid instruction at order {xml_instruction.get('order')}"
+
+                if instruction.order in orders:
+                    return None, 32, f"Duplicate instruction order {instruction.order}"
+
+                instructions.append(instruction)
+                orders.append(instruction.order)
+            else:
+                return None, 32, f"Invalid element '{xml_instruction.tag}' found"
+        
+        instructions.sort(key=lambda instr: instr.order)
+
+        return instructions, 0, None
+
+    def validate(self):
+        error_code, error_message = self.check_header()
+        if error_code:
+            return error_code, error_message
+
+        instructions, error_code, error_message = self.validate_instructions()
+        if error_code:
+            return error_code, error_message
+
+        return 0, None
 
 def main():
     args = argparser()
+    
+    if args.source:
+        try:
+            with open(args.source, "r") as file:
+                xml_string = file.read()
+        except IOError:
+            print(f"Error: Could not read file {args.source}", file=sys.stderr)
+            sys.exit(10)
+    else:
+        xml_string = sys.stdin.read()
+        
+    validator = XMLValidator(xml_string)
+    error_code, error_message = validator.validate()
+
+    if error_code:
+        print(f"XML.Error {error_code}: {error_message}", file=sys.stderr)
+        sys.exit(error_code)
 
 
 if __name__ == "__main__":
